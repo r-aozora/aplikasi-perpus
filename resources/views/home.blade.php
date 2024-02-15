24b1661dde6b41abc313462aae33b89f49889066
r-aozora <zytrahidayat11@gmail.com>
2024-02-16 02:42:36 +0700

    style: landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,12 +31,7 @@
                     <a class="nav-link active" href="#home">Home</a>
                     <a class="nav-link active" href="#buku">Buku</a>
                     <a class="nav-link active" href="#tentang">Tentang</a>
-                    @auth
-                        <a class="btn btn-primary" href="{{ route('dashboard') }}">Dashboard</a>
-                    @endauth
-                    @guest
-                        <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
-                    @endguest
+                    <a class="btn btn-primary" href="#">Login</a>
                 </div>
             </div>
         </div>
@@ -44,16 +39,72 @@
     {{-- Navbar End --}}
 
     {{-- Jumbotron Start --}}
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4"><span>Buku idamanmu</span><br> hanya sejauh <span>sentuhan jari</span></h1>
-            <a href="#" class="btn btn-primary">Lihat Buku</a>
+    <section id="home">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4" data-aos="fade-up"><span>Buku idamanmu</span><br> hanya sejauh <span>sentuhan jari</span></h1>
+                <a href="#" class="btn btn-primary">Mulai</a>
+            </div>
         </div>
-    </div>
+    </section>
     {{-- Jumbotron End --}}
+
+    {{-- Container --}}
+    <div class="container">
+        {{-- Book Panel --}}
+        <section id="buku">
+            <div class="row justify-content-center">
+                <div class="col-10 info-panel">
+                    <div class="row">
+                        <div class="col-lg">
+                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
+                            <h4>Nama Buku</h4>
+                            <p>Kategori</p>
+                        </div>
+                        <div class="col-lg">
+                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
+                            <h4>Nama Buku</h4>
+                            <p>Kategori</p>
+                        </div>
+                        <div class="col-lg">
+                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
+                            <h4>Nama Buku</h4>
+                            <p>Kategori</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{-- Book Panel End --}}
+
+        {{-- About --}}
+        <section id="tentang">
+            <div class="row workingspace">
+                <div class="col-lg-6">
+                    <img src="{{ asset('images/library-2.jpg') }}" alt="WorkingSpace" class="img-fluid">
+                </div>
+                <div class="col-lg-5">
+                    <h3>Pesan, <span>Pinjam</span>, Baca</h3>
+                    <p>PinjamBuku.id, tempat terbaik untuk memenuhi kebutuhan bacaan Anda dengan mudah dan praktis.</p>
+                    <a href="#" class="btn btn-primary">Pustaka</a>
+                </div>
+            </div>
+        </section>
+        {{-- About End --}}
+
+        {{-- Footer --}}
+        <div class="row footer">
+            <div class="col text-center">
+                <p>{{ date('Y') }} All Right Reserved by Muhamad Citra Hidayat.</p>
+            </div>
+        </div>
+        {{-- Footer End --}}
+    </div>
+    {{-- Container End --}}
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </body>
 
 </html>
