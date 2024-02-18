41f51a0d3055335f845debffd06d3dd68ff3c94c
r-aozora <zytrahidayat11@gmail.com>
2024-02-18 21:10:29 +0700

    refactor: landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>PinjamBuku.id</title>
+    <title>{{ config('app.name') }}</title>
 
     {{-- <!-- Bootstrap CSS --> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -22,7 +22,7 @@
     {{-- Navbar Start --}}
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="#">PinjamBuku.id</a>
+            <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -31,7 +31,12 @@
                     <a class="nav-link active" href="#home">Home</a>
                     <a class="nav-link active" href="#buku">Buku</a>
                     <a class="nav-link active" href="#tentang">Tentang</a>
-                    <a class="btn btn-primary" href="#">Login</a>
+                    @auth
+                        <a class="btn btn-primary" href="{{ route('dashboard') }}">Dashboard</a>
+                    @endauth
+                    @guest
+                        <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+                    @endguest
                 </div>
             </div>
         </div>
@@ -43,7 +48,7 @@
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
                 <h1 class="display-4" data-aos="fade-up"><span>Buku idamanmu</span><br> hanya sejauh <span>sentuhan jari</span></h1>
-                <a href="#" class="btn btn-primary">Mulai</a>
+                <a href="{{ route('pustaka.index') }}" class="btn btn-primary">Mulai</a>
             </div>
         </div>
     </section>
@@ -51,42 +56,75 @@
 
     {{-- Container --}}
     <div class="container">
-        {{-- Book Panel --}}
-        <section id="buku">
-            <div class="row justify-content-center">
-                <div class="col-10 info-panel">
-                    <div class="row">
-                        <div class="col-lg">
-                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
-                            <h4>Nama Buku</h4>
-                            <p>Kategori</p>
-                        </div>
-                        <div class="col-lg">
-                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
-                            <h4>Nama Buku</h4>
-                            <p>Kategori</p>
-                        </div>
-                        <div class="col-lg">
-                            <img src="{{ asset('images/buku.png') }}" alt="Buku" height="100" class="float-left">
-                            <h4>Nama Buku</h4>
-                            <p>Kategori</p>
-                        </div>
+        {{-- Info Panel --}}
+        <div class="row justify-content-center">
+            <div class="col-10 info-panel">
+                <div class="row">
+                    <div class="col-lg">
+                        <img src="{{ asset('images/bag.svg') }}" alt="Peminjaman" height="100" class="float-left">
+                        <h4>Peminjaman</h4>
+                        <p>Peminjaman cepat dan nyaman untuk menikmati buku impian.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="{{ asset('images/bookmarks.svg') }}" alt="Koleksi" height="100" class="float-left">
+                        <h4>Koleksi Buku</h4>
+                        <p>Telusuri koleksi buku kami yang kaya dan beragam.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="{{ asset('images/chat.svg') }}" alt="Ulasan" height="100" class="float-left">
+                        <h4>Ulasan</h4>
+                        <p>Dapatkan wawasan dan pandangan baru dengan ulasan buku.</p>
                     </div>
                 </div>
             </div>
+        </div>
+        {{-- Info Panel End --}}
+
+        {{-- Buku --}}
+        <section id="buku" class="buku">
+            <div class="row">
+                @forelse ($buku as $item)
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card">
+                            <img src="{{ asset($item->gambar) }}" alt="{{ $item->judul }}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $item->judul }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">{{ $item->kategori->kategori }}</h6>
+                                <p class="card-text">
+                                    @if (strlen($item->deskripsi) > 100)
+                                        {{ substr($item->deskripsi, 0, 100).'...' }}
+                                    @else
+                                        {{ $item->deskripsi }}
+                                    @endif
+                                </p>
+                                <a href="{{ route('pustaka.show', $item->slug) }}" class="card-link">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="col text-center">
+                        <h5>Buku Tidak Tersedia.</h5>
+                    </div>
+                @endforelse
+            </div>
+            <div class="row mt-5 {{ $buku ? '' : 'd-none' }}">
+                <div class="col text-center">
+                    <a href="{{ route('pustaka.index') }}" class="btn btn-primary">Lihat Semua</a>
+                </div>
+            </div>
         </section>
-        {{-- Book Panel End --}}
+        {{-- Buku End --}}
 
         {{-- About --}}
         <section id="tentang">
-            <div class="row workingspace">
+            <div class="row about">
                 <div class="col-lg-6">
-                    <img src="{{ asset('images/library-2.jpg') }}" alt="WorkingSpace" class="img-fluid">
+                    <img src="{{ asset('images/library-2.jpg') }}" alt="About" class="img-fluid">
                 </div>
                 <div class="col-lg-5">
                     <h3>Pesan, <span>Pinjam</span>, Baca</h3>
-                    <p>PinjamBuku.id, tempat terbaik untuk memenuhi kebutuhan bacaan Anda dengan mudah dan praktis.</p>
-                    <a href="#" class="btn btn-primary">Pustaka</a>
+                    <p>{{ config('app.name') }}, tempat terbaik untuk memenuhi kebutuhan bacaan Anda dengan mudah dan praktis.</p>
+                    <a href="{{ route('pustaka.index') }}" class="btn btn-primary">Pustaka</a>
                 </div>
             </div>
         </section>
@@ -104,7 +142,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </body>
 
 </html>
